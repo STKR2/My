6ac8c263b48eefe7cdd4d91f7930ce30070e09fc
levina <82658782+levina-lab@users.noreply.github.com>
2021-09-09 20:02:59 +0700

    fixes

--- bot/videoplayer.py
+++ bot/videoplayer.py
@@ -22,7 +22,7 @@ FFMPEG_PROCESSES = {}
 
 def raw_converter(dl, song, video):
     subprocess.Popen(
-        ['ffmpeg', '-i', dl, '-f', 's16le', '-ac', '1', '-ar', '48000', song, '-y', '-f', 'rawvideo', '-r', '20', '-pix_fmt', 'yuv420p', '-vf', 'scale=1280:720', video, '-y'],
+        ['ffmpeg', '-i', dl, '-f', 's16le', '-ac', '1', '-ar', '48000', song, '-y', '-f', 'rawvideo', '-r', '20', '-pix_fmt', 'yuv420p', '-vf', 'scale=854:480', video, '-y'],
         stdin=None,
         stdout=None,
         stderr=None,
