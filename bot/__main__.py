8b7d45c0d2602b78cdbfde1b854058183533a854
levina <82658782+levina-lab@users.noreply.github.com>
2021-09-09 10:05:12 +0700

    moved to py-tgcalls
    new file structure

--- bot/__main__.py
+++ bot/__main__.py
@@ -2,10 +2,11 @@
 # running bot
 # ===========
 
+import asyncio
 from pyrogram import Client, idle
 from config import API_ID, API_HASH, BOT_TOKEN
 from bot.videoplayer import app
-
+from player.videoplayer import call_py
 
 bot = Client(
     ":memory:",
@@ -19,5 +20,7 @@ bot.start()
 print("[INFO]: STARTING BOT CLIENT")
 app.start()
 print("[INFO]: STARTING USERBOT CLIENT")
+call_py.start()
+print("[INFO]: STARTING PY-TGCALLS CLIENT")
 idle()
 print("[INFO]: STOPPING BOT")
