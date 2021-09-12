9338a5f932f1f8f5ae0f20713c1b5d5cb4451381
zxce3 <memetpea25@gmail.com>
2021-09-12 11:37:01 +0700

    update filters.py

--- helpers/filters.py
+++ helpers/filters.py
@@ -2,11 +2,12 @@ from typing import Union, List
 
 from pyrogram import filters
 
-from config import COMMAND_PREFIXES
+# from config import Veez.COMMAND_PREFIXES
+from config import Veez
 
 other_filters = filters.group & ~filters.edited & ~filters.via_bot & ~filters.forwarded
 other_filters2 = filters.private & ~filters.edited & ~filters.via_bot & ~filters.forwarded
 
 
 def command(commands: Union[str, List[str]]):
-    return filters.command(commands, COMMAND_PREFIXES)
+    return filters.command(commands, Veez.COMMAND_PREFIXES)
