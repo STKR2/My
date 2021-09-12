52fc39423ad7b4f7caed608419de373494a3fc5f
zxce3 <memetpea25@gmail.com>
2021-09-12 11:37:21 +0700

    initialize update

--- config.py
+++ config.py
@@ -5,13 +5,19 @@ if path.exists("local.env"):
     load_dotenv("local.env")
 
 load_dotenv()
-admins = {}
-BOT_TOKEN = getenv("BOT_TOKEN", None)
-API_ID = int(getenv("API_ID", "6"))
-API_HASH = getenv("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
-SESSION_NAME = getenv("SESSION_NAME", None)
-DURATION_LIMIT = int(getenv("DURATION_LIMIT", "15"))
-SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
-ASSISTANT_NAME = getenv("ASSISTANT_NAME", "tg_video_stream")
-BOT_USERNAME = getenv("BOT_USERNAME", "veezvidstreambot")
-COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .").split())
+
+class Veez(object):
+        admins = {}
+        BOT_TOKEN = getenv("BOT_TOKEN", None)
+        API_ID = int(getenv("API_ID", "6"))
+        API_HASH = getenv("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
+        SESSION_NAME = getenv("SESSION_NAME", None)
+        DURATION_LIMIT = int(getenv("DURATION_LIMIT", "15"))
+        SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
+        ASSISTANT_NAME = getenv("ASSISTANT_NAME", "tg_video_stream")
+        BOT_USERNAME = getenv("BOT_USERNAME", "veezvidstreambot")
+        COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .").split())
+        FPS = int(getenv('FPS', 20))
+        WIDTH = int(getenv('WIDTH', 1280))
+        HEIGHT = int(getenv('HEIGHT', 720))
+        BITRATE = int(getenv('BITRATE', 48000))
