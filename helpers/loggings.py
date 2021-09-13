cb727e36c35fa9c46bb5c16587be4934cd9451c5
zxce3 <memetpea25@gmail.com>
2021-09-13 15:24:41 +0700

    add logging

---
+++ helpers/loggings.py
@@ -0,0 +1,14 @@
+import logging
+import time
+
+
+logging.basicConfig(
+    filename='bot.log',
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+logging.getLogger("pyrogram").setLevel(logging.ERROR)
+logging.getLogger("PyTgCalls").setLevel(logging.WARNING)
+logging.getLogger("youtube_dl").setLevel(logging.WARNING)
+
+LOG = logging.getLogger(__name__)
