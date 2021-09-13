dc0b7415bf2a1003f795301bd364b4664c17de25
zxce3 <memetpea25@gmail.com>
2021-09-14 04:45:09 +0700

    forgot commit

--- bot/song.py
+++ bot/song.py
@@ -12,7 +12,7 @@ import aiofiles
 import aiohttp
 import requests
 import youtube_dl
-from youtube_dl import YoutubeDL
+from yt_dlp import YoutubeDL
 from pyrogram import Client, filters
 from pyrogram.errors import FloodWait, MessageNotModified
 from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
