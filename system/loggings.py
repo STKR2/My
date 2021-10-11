1c8fd6611b6ace6231a7440422321722c9329488
levina <82658782+levina-lab@users.noreply.github.com>
2021-10-11 10:13:34 +0700

    setup
    change to ytdlp

--- system/loggings.py
+++ system/loggings.py
@@ -11,6 +11,6 @@ logging.basicConfig(
 )
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
 logging.getLogger("PyTgCalls").setLevel(logging.WARNING)
-logging.getLogger("youtube_dl").setLevel(logging.WARNING)
+logging.getLogger("yt_dlp").setLevel(logging.WARNING)
 
 LOG = logging.getLogger(__name__)
