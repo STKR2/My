43e591f34e2ee664c23041c5db57b8ae95cee71c
levina <82658782+levina-lab@users.noreply.github.com>
2021-10-11 11:33:42 +0700

    setup

---
+++ system/software.py
@@ -0,0 +1,13 @@
+# Copyright (C) 2021 Veez Music-Project
+
+from config import Config
+from pyrogram import Client
+from pytgcalls import PyTgCalls
+
+VGA = Client(
+    Config.SESSION,
+    Config.API_ID,
+    Config.API_HASH,
+    plugins=dict(root="program")
+    )
+group_call = PyTgCalls(VGA, cache_duration=180)
