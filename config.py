2d14fb2ca551f54bd5efab7116580c67dced89f0
levina <82658782+levina-lab@users.noreply.github.com>
2021-10-26 05:21:15 +0700

    some fixes

--- config.py
+++ config.py
@@ -1,26 +1,23 @@
-# Copyright (C) 2021 By VeezMusicProject
-
 import os
-from os import path, getenv
+from os import getenv
 from dotenv import load_dotenv
 
-if path.exists("local.env"):
+if os.path.exists("local.env"):
     load_dotenv("local.env")
 
 load_dotenv()
-
-class Veez(object):
-        admins = {}
-        BOT_TOKEN = getenv("BOT_TOKEN", None)
-        CHANNEL = int(os.getenv('CHANNEL', 123456))
-        API_ID = int(getenv("API_ID", "6"))
-        API_HASH = getenv("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
-        SESSION_NAME = getenv("SESSION_NAME", None)
-        DURATION_LIMIT = int(getenv("DURATION_LIMIT", "15"))
-        SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
-        ASSISTANT_NAME = getenv("ASSISTANT_NAME", "tg_video_stream")
-        BOT_USERNAME = getenv("BOT_USERNAME", "veezvidstreambot")
-        COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .").split())
-        CHANNEL_NAME = getenv("CHANNEL_NAME", "levinachannel")
-        GROUP_NAME = getenv("GROUP_NAME", "VeezSupportGroup")
-        OWNER_NAME = getenv("OWNER_NAME", "dlwrml")
+admins = {}
+SESSION_NAME = getenv("SESSION_NAME", "session")
+BOT_TOKEN = getenv("BOT_TOKEN")
+BOT_NAME = getenv("BOT_NAME", "Video Stream")
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
+OWNER_NAME = getenv("OWNER_NAME", "dlwrml")
+ALIVE_NAME = getenv("ALIVE_NAME", "Levina")
+BOT_USERNAME = getenv("BOT_USERNAME", "veezvideobot")
+ASSISTANT_NAME = getenv("ASSISTANT_NAME", "cleo_invida")
+GROUP_SUPPORT = getenv("GROUP_SUPPORT", "VeezSupportGroup")
+UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "levinachannel")
+SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
+COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .").split())
+ALIVE_IMG = getenv("ALIVE_IMG", "https://telegra.ph/file/c83b000f004f01897fe18.png")
