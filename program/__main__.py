5569aabe4254afddcf8fae714d0c29489c5b3f6e
levina <82658782+levina-lab@users.noreply.github.com>
2021-10-25 15:24:50 +0700

    forgot

--- program/__main__.py
+++ program/__main__.py
@@ -2,7 +2,7 @@ import os
 from pyrogram import Client, idle
 from pytgcalls import PyTgCalls
 from pytgcalls import idle as engine
-from driver import bot, call_py
+from driver.veez import bot, call_py
 from program import __version__
 
 bot.start()
