c8047a6308b2e05ddd4de393258f2b6da3f25c54
levina <82658782+levina-lab@users.noreply.github.com>
2021-10-26 13:37:52 +0700

    no need

--- driver/veez.py
+++ driver/veez.py
@@ -1,11 +1,6 @@
+from pyrogram import Client
 from pytgcalls import PyTgCalls
-from pyrogram import Client, filters
 from config import SESSION_NAME, API_ID, API_HASH
-
-contact_filter = filters.create(
-    lambda _, __, message:
-    (message.from_user and message.from_user.is_contact) or message.outgoing
-)
 
 bot = Client(
   SESSION_NAME,
@@ -13,4 +8,5 @@ bot = Client(
   API_HASH,
   plugins=dict(root="program")
 )
+
 call_py = PyTgCalls(bot)
