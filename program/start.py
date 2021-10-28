388a60cf6e3e90224620ebbd2387a3546ba9c7ae
levina <82658782+levina-lab@users.noreply.github.com>
2021-10-28 18:07:18 +0700

    none

--- program/start.py
+++ program/start.py
@@ -54,11 +54,12 @@ async def _human_time_duration(seconds):
 async def start_(client: Client, message: Message):
     await message.reply_text(
         f"""✨ **Welcome {message.from_user.mention()} !**\n
-💭 **[{BOT_NAME}](https://t.me/{BOT_USERNAME}) allows you to play music and video on groups through the new Telegram's video chats!**
+💭 [{BOT_NAME}](https://t.me/{BOT_USERNAME}) **allows you to play music and video on groups through the new Telegram's video chats!**
 
 💡 **Find out all the Bot's commands and how they work by clicking on the » 📚 Commands button!**
 
-❔ **To know how to use this bot, please click on the » ❓ Basic Guide button!**""",
+❔ **To know how to use this bot, please click on the » ❓ Basic Guide button!**
+""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
