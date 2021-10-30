5cba15abf9f54201986c6f7eb87d1a890f59dd04
Tofik Denianto <77754555+tofikdn@users.noreply.github.com>
2021-10-31 05:53:27 +0700

    Update veez.py

--- driver/veez.py
+++ driver/veez.py
@@ -1,13 +1,19 @@
+from config import API_HASH, API_ID, BOT_TOKEN, SESSION_NAME
 from pyrogram import Client
 from pytgcalls import PyTgCalls
-from config import SESSION_NAME, API_ID, API_HASH, BOT_TOKEN
 
 bot = Client(
-  ":memory:",
-  API_ID,
-  API_HASH,
-  bot_token=BOT_TOKEN,
-  plugins=dict(root="program")
+    ":memory:",
+    API_ID,
+    API_HASH,
+    bot_token=BOT_TOKEN,
+    plugins={"root": "program"},
 )
-user = Client(SESSION_NAME, API_ID, API_HASH)
+
+user = Client(
+    SESSION_NAME,
+    api_id=API_ID,
+    api_hash=API_HASH,
+)
+
 call_py = PyTgCalls(user)
