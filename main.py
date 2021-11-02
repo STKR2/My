843ef7e5056eb0c0267277b9a737e538404dca39
levina <82658782+levina-lab@users.noreply.github.com>
2021-11-02 15:36:57 +0700

    something

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 import asyncio
 
+from program import BOT_ID, USERBOT_ID
 from driver.veez import bot, call_py
 from pytgcalls import idle
+
+
+def all_info(bot, call_py):
+    global BOT_ID, USERBOT_ID
+    getme = bot.get_me()
+    getme1 = call_py.get_me()
+    BOT_ID = getme.id
+    USERBOT_ID = getme1.id
 
 
 async def mulai_bot():
@@ -9,6 +18,8 @@ async def mulai_bot():
     await bot.start()
     print("[INFO]: STARTING PYTGCALLS CLIENT")
     await call_py.start()
+    print("[INFO]: GENERATING CLIENT PROFILE")
+    all_info(bot, call_py)
     await idle()
     print("[INFO]: STOPPING BOT")
     await bot.stop()
