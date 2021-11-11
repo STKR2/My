c1b3ac447fd82aa494836409db49299ed7df7684
levina <82658782+levina-lab@users.noreply.github.com>
2021-11-12 06:30:14 +0700

    setup

--- program/callback.py
+++ program/callback.py
@@ -1,6 +1,7 @@
 # Copyright (C) 2021 By VeezMusicProject
 
 from pyrogram import Client, filters
+from driver.queues import QUEUE
 from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
 from config import (
     ASSISTANT_NAME,
@@ -170,21 +171,26 @@ async def cbmenu(_, query: CallbackQuery):
     a = await _.get_chat_member(query.message.chat.id, query.from_user.id)
     if not a.can_manage_voice_chats:
         return await query.answer("💡 only admin with manage voice chats permission that can tap this button !", show_alert=True)
-    await query.edit_message_text(
-        f"⚙️ **settings of** {query.message.chat.title}\n\n⏸ : pause stream\n▶️ : resume stream\n🔇 : mute userbot\n🔊 : unmute userbot\n⏹ : stop stream",
-        reply_markup=InlineKeyboardMarkup(
-            [[
-                InlineKeyboardButton("⏹", callback_data="cbstop"),
-                InlineKeyboardButton("⏸", callback_data="cbpause"),
-                InlineKeyboardButton("▶️", callback_data="cbresume"),
-            ],[
-                InlineKeyboardButton("🔇", callback_data="cbmute"),
-                InlineKeyboardButton("🔊", callback_data="cbunmute"),
-            ],[
-                InlineKeyboardButton("🗑 Close", callback_data="cls")],
-            ]
-        ),
-    )
+    chat_id = query.message.chat.id
+    if chat_id in QUEUE:
+        try:
+            await query.edit_message_text(
+                f"⚙️ **settings of** {query.message.chat.title}\n\n⏸ : pause stream\n▶️ : resume stream\n🔇 : mute userbot\n🔊 : unmute userbot\n⏹ : stop stream",
+                reply_markup=InlineKeyboardMarkup(
+                    [[
+                        InlineKeyboardButton("⏹", callback_data="cbstop"),
+                        InlineKeyboardButton("⏸", callback_data="cbpause"),
+                        InlineKeyboardButton("▶️", callback_data="cbresume"),
+                    ],[
+                        InlineKeyboardButton("🔇", callback_data="cbmute"),
+                        InlineKeyboardButton("🔊", callback_data="cbunmute"),
+                    ],[
+                        InlineKeyboardButton("🗑 Close", callback_data="cls")],
+                    ]
+                ),
+            )
+    else:
+        await query.answer("❌ nothing is currently streaming", show_alert=True)
 
 
 @Client.on_callback_query(filters.regex("cls"))
