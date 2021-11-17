9a3679d87906f34f6ebd13d1dbd0dcd07d65325b
levina <82658782+levina-lab@users.noreply.github.com>
2021-11-17 13:57:45 +0700

    something

--- program/playlist.py
+++ program/playlist.py
@@ -25,7 +25,7 @@ async def playlist(client, m: Message):
    if chat_id in QUEUE:
       chat_queue = get_queue(chat_id)
       if len(chat_queue)==1:
-         await m.reply(f"💡 **Currently Streaming:**\n\n• [{chat_queue[0][0]}]({chat_queue[0][2]}) | `{chat_queue[0][3]}`", reply_markup=keyboard)
+         await m.reply(f"💡 **Currently Streaming:**\n\n• [{chat_queue[0][0]}]({chat_queue[0][2]}) | `{chat_queue[0][3]}`", reply_markup=keyboard, disable_web_page_preview=True)
       else:
          QUE = f"💡 **Currently Streaming:**\n\n• [{chat_queue[0][0]}]({chat_queue[0][2]}) | `{chat_queue[0][3]}` \n\n**📖 Queue List:**\n"
          l = len(chat_queue)
@@ -34,6 +34,6 @@ async def playlist(client, m: Message):
             hok = chat_queue[x][2]
             hap = chat_queue[x][3]
             QUE = QUE + "\n" + f"**#{x}** - [{han}]({hok}) | `{hap}`"
-         await m.reply(QUE, reply_markup=keyboard)
+         await m.reply(QUE, reply_markup=keyboard, disable_web_page_preview=True)
    else:
       await m.reply("❌ **nothing is currently streaming.**")
