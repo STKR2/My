fbf4c2116d30e327a38b5aef88257bfced51f34e
levina <82658782+levina-lab@users.noreply.github.com>
2021-12-11 15:43:37 +0700

    fixes

--- program/music.py
+++ program/music.py
@@ -9,6 +9,7 @@ from config import ASSISTANT_NAME, BOT_USERNAME, IMG_1, IMG_2
 from driver.filters import command, other_filters
 from driver.queues import QUEUE, add_to_queue
 from driver.veez import call_py, user
+from driver.utils import bash
 from pyrogram import Client
 from pyrogram.errors import UserAlreadyParticipant, UserNotParticipant
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
