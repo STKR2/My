cc23b777360d400be72a009f292a3f6ed77757b7
levina <82658782+levina-lab@users.noreply.github.com>
2022-01-15 21:05:09 +0700

    something

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from pytgcalls import idle
 from driver.veez import call_py, bot
 
 async def start_bot():
-    print("[INFO]: STARTING BOT CLIENT")
     await bot.start()
-    print("[INFO]: STARTING PYTGCALLS CLIENT")
+    print("[INFO]: BOT & UBOT CLIENT STARTED !!")
     await call_py.start()
+    print("[INFO]: PYTGCALLS CLIENT STARTED !!")
     await idle()
     print("[INFO]: STOPPING BOT & USERBOT")
     await bot.stop()
