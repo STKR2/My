b955eb362523a065ec23e289bf01beafc6aeff14
levina <82658782+levina-lab@users.noreply.github.com>
2022-01-18 12:54:12 +0700

    something

--- program/admins.py
+++ program/admins.py
@@ -66,7 +66,7 @@ async def skip(client, m: Message):
             await m.reply("🗑️ **Clearing the Queues**\n\n**• userbot leaving voice chat**")
         else:
             await m.reply_photo(
-                photo=f"{IMG_3}",
+                photo=f"{op[2]}",
                 caption=f"⏭ **Skipped to the next track.**\n\n🏷 **Name:** [{op[0]}]({op[1]})\n💭 **Chat:** `{chat_id}`\n💡 **Status:** `Playing`\n🎧 **Request by:** {m.from_user.mention()}",
                 reply_markup=keyboard,
             )
