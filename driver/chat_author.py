74d27098e36481482db8b6c723c23ecf6fd1b72d
levina <82658782+levina-lab@users.noreply.github.com>
2022-01-19 21:23:20 +0700

    some fixes

--- driver/chat_author.py
+++ driver/chat_author.py
@@ -9,32 +9,32 @@ from program.admins import member_permissions
 from pyrogram.errors.exceptions.forbidden_403 import ChatWriteForbidden
 
 
-async def authorised(message):
+async def authorised(m: Message):
     chatID = message.chat.id
     return 0
 
 
-async def unauthorised(message: Message):
-    chatID = message.chat.id
+async def unauthorised(m: Message):
+    chatID = m.chat.id
     text = (
         "You're missing admin rights to use this command."
         + f"\n\n» ❌ can_manage_voice_chats"
     )
     try:
-        await message.reply_text(text)
+        await m.reply_text(text)
     except ChatWriteForbidden:
         await bot.leave_chat(chatID)
     return 1
 
   
-async def adminsOnly(permission, message):
-    chatID = message.chat.id
-    if not message.from_user:
-        if message.sender_chat:
-            return await authorised(message)
-        return await unauthorised(message)
-    userID = message.from_user.id
+async def adminsOnly(permission, m: Message):
+    chatID = m.chat.id
+    if not m.from_user:
+        if m.sender_chat:
+            return await authorised(m)
+        return await unauthorised(m)
+    userID = m.from_user.id
     permissions = await member_permissions(chatID, userID)
     if userID not in SUDO_USERS and permission not in permissions:
-        return await unauthorised(message)
-    return await authorised(message)
+        return await unauthorised(m)
+    return await authorised(m)
