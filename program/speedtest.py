0594b06e4feda06b108aea557bef3aa4c7a21524
levina <82658782+levina-lab@users.noreply.github.com>
2022-01-20 22:17:08 +0700

    fixes

--- program/speedtest.py
+++ program/speedtest.py
@@ -8,7 +8,7 @@ from program.utils.formatters import bytes
 from driver.filters import command, other_filters
 from driver.decorators import sudo_users_only
 from config import BOT_USERNAME as bname
-from driver.utils import bot as app
+from driver.veez import bot as app
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
