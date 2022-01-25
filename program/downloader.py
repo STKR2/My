6a213c3ae2f8ad91cac7307f2924fdb6185a471d
levina <82658782+levina-lab@users.noreply.github.com>
2022-01-25 14:39:05 +0700

    none

--- program/downloader.py
+++ program/downloader.py
@@ -63,7 +63,7 @@ def song(_, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f"**🎧 Uploader @{bn}**"
+        rep = f"• uploader @{bn}"
         secmul, dur, dur_arr = 1, 0, duration.split(":")
         for i in range(len(dur_arr) - 1, -1, -1):
             dur += int(float(dur_arr[i])) * secmul
