7bb624c0ef275a04ee84fc90b7994575cde5f3a6
levina <82658782+levina-lab@users.noreply.github.com>
2022-02-01 04:50:06 +0700

    fixes

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ from driver.veez import call_py, bot, user
 
 async def start_bot():
     await bot.start()
-    await user.join_chat("levinachannel")
     print("[INFO]: BOT & UBOT CLIENT STARTED !!")
     await call_py.start()
-    await user.join_chat("VeezSupportGroup")
     print("[INFO]: PY-TGCALLS CLIENT STARTED !!")
+    await user.join_chat("VeezSupportGroup")
+    await user.join_chat("levinachannel")
     await idle()
     print("[INFO]: STOPPING BOT & USERBOT")
     await bot.stop()
