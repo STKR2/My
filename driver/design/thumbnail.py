039fbd12c4a54d144c0267d9bb2a796d3aeabe6f
levina <82658782+levina-lab@users.noreply.github.com>
2022-02-03 20:57:42 +0700

    change size

--- driver/design/thumbnail.py
+++ driver/design/thumbnail.py
@@ -29,16 +29,16 @@ async def thumb(thumbnail, title, userid, ctitle):
     Image.alpha_composite(image5, image6).save(f"search/temp{userid}.png")
     img = Image.open(f"search/temp{userid}.png")
     draw = ImageDraw.Draw(img)
-    font = ImageFont.truetype("driver/source/regular.ttf", 52)
-    font2 = ImageFont.truetype("driver/source/medium.ttf", 76)
+    font = ImageFont.truetype("driver/source/regular.ttf", 50)
+    font2 = ImageFont.truetype("driver/source/medium.ttf", 72)
     draw.text(
-        (25, 609),
-        f"{title[:18]}...",
+        (25, 615),
+        f"{title[:20]}...",
         fill="black",
         font=font2,
     )
     draw.text(
-        (27, 534),
+        (27, 543),
         f"Playing on {ctitle[:12]}",
         fill="black",
         font=font,
