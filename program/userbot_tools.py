3a081cc1288ba90801ed8d7c91d40773ae16753b
levina <82658782+levina-lab@users.noreply.github.com>
2022-02-06 12:51:38 +0700

    re-count
    after bot kicked from the group the total chat will re-counted again

--- program/userbot_tools.py
+++ program/userbot_tools.py
@@ -4,6 +4,7 @@ from pyrogram.types import Message
 from pyrogram import Client, filters
 from config import BOT_USERNAME, SUDO_USERS
 from driver.filters import command, other_filters
+from driver.database.dbchat import remove_served_chat
 from pyrogram.errors import UserAlreadyParticipant, UserNotParticipant
 from driver.decorators import authorized_users_only, sudo_users_only
 
@@ -75,12 +76,11 @@ async def leave_all(client, message):
 
 
 @Client.on_message(filters.left_chat_member)
-async def ubot_leave(c: Client, m: Message):
-#    ass_id = (await user.get_me()).id
+async def bot_kicked(c: Client, m: Message):
     bot_id = (await c.get_me()).id
     chat_id = m.chat.id
     left_member = m.left_chat_member
     if left_member.id == bot_id:
         await user.leave_chat(chat_id)
-#    elif left_member.id == ass_id:
-#        await c.leave_chat(chat_id)
+        return
+    await remove_served_chat(chat_id)
