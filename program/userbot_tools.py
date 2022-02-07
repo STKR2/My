ab0d0f20a426a865919e85ab4880f2789c7610b0
levina <82658782+levina-lab@users.noreply.github.com>
2022-02-07 23:19:25 +0700

    change name

--- program/userbot_tools.py
+++ program/userbot_tools.py
@@ -1,5 +1,5 @@
 import asyncio
-from driver.veez import user
+from driver.core import user
 from pyrogram.types import Message
 from pyrogram import Client, filters
 from config import BOT_USERNAME, SUDO_USERS
