7b8f35ee815b78c50043f35f5c3ed7baab26904a
levina <82658782+levina-lab@users.noreply.github.com>
2022-02-07 10:17:26 +0700

    none

--- program/extra.py
+++ program/extra.py
@@ -127,7 +127,7 @@ async def broadcast(c: Client, message: Message):
     chat_id = message.chat.id
     user_id = message.from_user.id
     msg = await c.send_message(
-        chat_id, "🔄 `Calculating statistic...`"
+        chat_id, "🔄 Calculate statistics..."
     )
     served_chats = len(await get_served_chats())
     served_users = len(await get_served_users())
