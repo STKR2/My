e242681ab20f90e4461499a6abbb42bb7686c8c1
levina <82658782+levina-lab@users.noreply.github.com>
2022-02-16 13:38:20 +0700

    change format

--- program/music.py
+++ program/music.py
@@ -46,7 +46,7 @@ def ytsearch(query: str):
 
 async def ytdl(link: str):
     stdout, stderr = await bash(
-        f'yt-dlp -g -f "best[height<=?720][width<=?1280]" {link}'
+        f'yt-dlp --geo-bypass -g -f "best[height<=?720][width<=?1280]/best" {link}'
     )
     if stdout:
         return 1, stdout
