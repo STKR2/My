cee28daf51c86f75591f5a309f73938b61759f74
levina <82658782+levina-lab@users.noreply.github.com>
2022-02-19 07:27:47 +0700

    some info

---
+++ driver/database/__init__.py
@@ -0,0 +1,4 @@
+""" database copyright """
+
+# thanks to TheHamkerCat (https://github.com/TheHamkerCat) for his WilliamButcherBot database.
+# implemented by levina-lab (https://github.com/levina-lab)
