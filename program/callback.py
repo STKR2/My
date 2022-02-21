c3b274379562f993e34a60f11226d267ca2f5d90
levina <82658782+levina-lab@users.noreply.github.com>
2022-02-21 12:20:37 +0700

    command info

--- program/callback.py
+++ program/callback.py
@@ -186,6 +186,7 @@ async def admin_set(_, query: CallbackQuery):
 » /reload - reload bot and refresh the admin data
 » /userbotjoin - invite the userbot to join group
 » /userbotleave - order userbot to leave from group
+» /startvc - start/restart the group call
 
 ⚡️ __Powered by {BOT_NAME} AI__""",
         reply_markup=InlineKeyboardMarkup(
