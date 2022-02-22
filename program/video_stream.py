9833ce3d230c3852d4070b44b3bf8c97f5721091
levina <82658782+levina-lab@users.noreply.github.com>
2022-02-23 06:42:50 +0700

    copyright header

--- program/video_stream.py
+++ program/video_stream.py
@@ -1,12 +1,26 @@
-# Copyright (C) 2021 By Veez Music-Project
-# Commit Start Date 20/10/2021
-# Finished On 28/10/2021
+"""
+Video + Music Stream Telegram Bot
+Copyright (c) 2022-present levina=lab <https://github.com/levina-lab>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but without any warranty; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/licenses.html>
+"""
 
 
 import re
 import asyncio
 import traceback
-# repository stuff
+
 from config import BOT_USERNAME, IMG_1, IMG_2, IMG_5
 from driver.decorators import require_admin, check_blacklist
 from program.utils.inline import stream_markup
@@ -17,11 +31,11 @@ from driver.queues import QUEUE, add_to_queue
 from driver.core import calls, user, me_user
 from driver.database.dbqueue import add_active_chat, remove_active_chat, music_on
 from driver.utils import remove_if_exists, from_tg_get_msg
-# pyrogram stuff
+
 from pyrogram import Client
 from pyrogram.errors import UserAlreadyParticipant, UserNotParticipant
 from pyrogram.types import InlineKeyboardMarkup, Message
-# py-tgcalls stuff
+
 from pytgcalls import idle
 from pytgcalls import StreamType
 from pytgcalls.types.input_stream import AudioVideoPiped
@@ -31,7 +45,7 @@ from pytgcalls.types.input_stream.quality import (
     LowQualityVideo,
     MediumQualityVideo,
 )
-# youtube-dl stuff
+
 from youtubesearchpython import VideosSearch
 
 
