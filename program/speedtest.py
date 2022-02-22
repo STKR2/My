91b238a2c93edb499960c24e2a60a587ba5fec94
levina <82658782+levina-lab@users.noreply.github.com>
2022-02-23 06:37:00 +0700

    copyright header

--- program/speedtest.py
+++ program/speedtest.py
@@ -1,14 +1,33 @@
-# credit to TeamYukki for this speedtest module
+"""
+Video + Music Stream Telegram Bot
+Copyright (c) 2022-present levina=lab <https://github.com/levina-lab>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but without any warranty; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/licenses.html>
+"""
+
 
 import wget
 import speedtest
 
 from PIL import Image
+from config import BOT_USERNAME as bname
+
 from driver.filters import command
 from driver.decorators import sudo_users_only
-from config import BOT_USERNAME as bname
 from driver.core import bot as app
 from driver.utils import remove_if_exists
+
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
