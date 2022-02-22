f22cd78c6df56615fbca849fe0b5a6bccac0dade
levina <82658782+levina-lab@users.noreply.github.com>
2022-02-23 06:33:53 +0700

    copyright header

--- program/playlist.py
+++ program/playlist.py
@@ -1,14 +1,30 @@
-# Copyright (C) 2021 By Veez Music-Project
-# Commit Start Date 20/10/2021
-# Finished On 28/10/2021
+"""
+Video + Music Stream Telegram Bot
+Copyright (c) 2022-present levina=lab <https://github.com/levina-lab>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but without any warranty; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/licenses.html>
+"""
+
 
 from config import BOT_USERNAME
+
+from pyrogram import Client
 from pyrogram.types import (
     InlineKeyboardButton,
     InlineKeyboardMarkup,
     Message,
 )
-from pyrogram import Client
 
 from driver.decorators import check_blacklist
 from driver.queues import QUEUE, get_queue
