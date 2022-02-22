f6a2ba627de623a0fc3577b86164131f1232ae2f
levina <82658782+levina-lab@users.noreply.github.com>
2022-02-23 06:16:07 +0700

    copyright header

--- program/admins.py
+++ program/admins.py
@@ -1,3 +1,22 @@
+"""
+Video + Music Stream Telegram Bot
+Copyright (c) 2022-present levina=lab <https://github.com/levina-lab>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but without any warranty; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/licenses.html>
+"""
+
+
 import traceback
 
 from cache.admins import admins
