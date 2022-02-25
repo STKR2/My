98bf36ec00049d689a76734fada77f7853f671a4
levina <82658782+levina-lab@users.noreply.github.com>
2022-02-25 10:53:23 +0700

    [set] setup logger

--- program/__init__.py
+++ program/__init__.py
@@ -1,3 +1,16 @@
 __version__ = "0.6.5"
+# >>> patch : F.11.22
 
-# update patch : F.11.22
+import time
+import logging
+
+logging.basicConfig(
+  filename='bot.logs',
+  level=logging.INFO,
+  format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+logging.getLogger("yt_dlp").setLevel(logging.ERROR)
+logging.getLogger("pyrogram").setLevel(logging.ERROR)
+logging.getLogger("PyTgCalls").setLevel(logging.ERROR)
+
+LOGS = logging.getLogger(__name__)
