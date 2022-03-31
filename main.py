b782f4a8de8f09d00831868c4c54223f2f23942f
levina <82658782+levina-lab@users.noreply.github.com>
2022-03-31 07:29:29 +0700

    [+] some additional changes

--- main.py
+++ main.py
@@ -2,21 +2,19 @@ import asyncio
 
 from program import LOGS
 from pytgcalls import idle
-from config import BOT_USERNAME as cli
 from driver.core import calls, bot, user
 
 
 async def start_bot():
     await bot.start()
-    LOGS.info("[INFO]: BOT & USERBOT CLIENT STARTED !!")
+    LOGS.info("[ INFO ] BOT & USERBOT CLIENT STARTED")
     await calls.start()
-    LOGS.info("[INFO]: PY-TGCALLS CLIENT STARTED !!")
+    LOGS.info("[ INFO ] PY-TGCALLS CLIENT STARTED")
     await user.join_chat("VeezSupportGroup")
     await user.join_chat("levinachannel")
-    await user.send_message(f"{cli}", "/start")
     await idle()
-    LOGS.info("[INFO]: BOT & USERBOT STOPPED !!")
+    LOGS.info("[ INFO ] BOT & USERBOT CLIENT STOPPED")
     await bot.stop()
 
-loop = asyncio.get_event_loop()
+loop = asyncio.get_event_loop_policy().get_event_loop()
 loop.run_until_complete(start_bot())
