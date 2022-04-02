e1917b805e4a3938527fdee4b21b7fbaf7b22f25
levina <82658782+levina-lab@users.noreply.github.com>
2022-04-02 14:08:35 +0700

    [feat] reformat main file

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import asyncio
 
 from program import LOGS
 from pytgcalls import idle
-from config import BOT_USERNAME as cli
 from driver.core import calls, bot, user, lang_
 
 
@@ -13,10 +12,9 @@ async def start_bot():
     LOGS.info(lang_.get("pytgcall_started"))
     await user.join_chat("VeezSupportGroup")
     await user.join_chat("levinachannel")
-    await user.send_message(f"{cli}", "/start")
     await idle()
     LOGS.info(lang_.get("client_stopped"))
     await bot.stop()
 
-loop = asyncio.get_event_loop()
+loop = asyncio.get_event_loop_policy().get_event_loop()
 loop.run_until_complete(start_bot())
