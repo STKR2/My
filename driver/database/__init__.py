cb52d6caa5cfaa9d8f4b8d895e36e3c58c250f0e
levina <82658782+levina-lab@users.noreply.github.com>
2022-04-07 14:53:19 +0700

    remove old file

--- driver/database/__init__.py
+++
@@ -1,4 +0,0 @@
-""" database copyright """
-
-# thanks to TheHamkerCat (https://github.com/TheHamkerCat) for his WilliamButcherBot database.
-# implemented by levina-lab (https://github.com/levina-lab)
