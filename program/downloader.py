1707d066cdadf6949fb7b3980fd09c9f623e23d4
X02lx <100516396+X02lx@users.noreply.github.com>
2023-01-19 04:12:06 +0300

    Update downloader.py

--- program/downloader.py
+++ program/downloader.py
@@ -60,7 +60,7 @@ def song(_, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f"**🎧 تم التحميل بواسطة **"
+        rep = f"**🎧 تم التحميل بواسطة @RR8R9**"
         secmul, dur, dur_arr = 1, 0, duration.split(":")
         for i in range(len(dur_arr) - 1, -1, -1):
             dur += int(float(dur_arr[i])) * secmul
